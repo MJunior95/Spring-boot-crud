6c72ec393503db46e409be4b5a35a48434793174
Michel Junior <jrcassunde95@gmail.com>
2021-12-13 14:21:58 -0300

    primeiro commit

---
+++ src/test/java/br/com/springboot/jdevtreinamento/JdevTreinamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.springboot.jdevtreinamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdevTreinamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
